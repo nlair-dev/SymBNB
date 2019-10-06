d6864f244dbe22c8535d8c4d8497c856cc44b057
Nicolas Lair <mch@nicolaslair.com>
2019-10-06 14:13:38 +0200

    Premier commit, installation et page d'accueil

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends Controller
+{
+    /**
+     * Home function
+     * @Route("/", name="homepage")
+     * @return Response
+     */
+    public function home(): Response
+    {
+        $prenoms = ["Lior" => 31, "Joseph" => 12, "Anne" => 55];
+        
+        return $this->render('home.html.twig', [
+                'title' => "Bonjour à tous",
+                'age' => 8,
+                'tableau' => $prenoms
+            ]
+        );
+    }
+
+    /**
+     * Show hello page with a name for parameter
+     * @Route("/hello/{prenom}/age/{age}", name="hello")
+     * @Route("/hello", name="hello_base")
+     * @Route("/hello/{prenom}", name="hello_prenom")
+     * @return Response
+     */
+    public function hello(string $prenom = 'anonyme', $age = 0): Response 
+    {
+        return $this->render('hello.html.twig', [
+            'prenom' => $prenom,
+            'age' => $age,
+        ]);
+    }
+}
